1b2517545ef5e19e9a5d15d63364e164f761bf14
truecode112 <truecoder112@gmail.com>
2023-02-04 01:37:10 +0900

    Completed Create, Update Customer Profile Charge Credit Card and Customer Profile

---
+++ CustomerProfiles/CreateCustomerPaymentProfile.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers.Bases;
+
+namespace AuthorizePayment.CustomerProfiles
+{
+    public class CreateCustomerPaymentProfile
+    {
+        public static ANetApiResponse Run(string ApiLoginID, string ApiTransactionKey, string customerProfileId)
+        {
+            Console.WriteLine("Create Customer Payment Profile Sample");
+
+            // set whether to use the sandbox environment, or production enviornment
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+
+            // define the merchant information (authentication / transaction id)
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
+            {
+                name            = ApiLoginID,
+                ItemElementName = ItemChoiceType.transactionKey,
+                Item            = ApiTransactionKey,
+            };
+
+            var bankAccount = new bankAccountType
+            {
+                accountNumber = "01245524321",
+                routingNumber = "000000204",
+                accountType = bankAccountTypeEnum.checking,
+                echeckType = echeckTypeEnum.WEB,
+                nameOnAccount = "test",
+                bankName = "Bank Of America"
+            };
+
+            paymentType echeck = new paymentType {Item = bankAccount};
+
+            var billTo = new customerAddressType
+            {
+                firstName = "John",
+                lastName = "Snow"
+            };
+            customerPaymentProfileType echeckPaymentProfile = new customerPaymentProfileType();
+            echeckPaymentProfile.payment = echeck;
+            echeckPaymentProfile.billTo = billTo;
+
+            var request = new createCustomerPaymentProfileRequest
+            {
+                customerProfileId = customerProfileId,
+                paymentProfile = echeckPaymentProfile,
+                validationMode = validationModeEnum.none
+            };
+
+            // instantiate the controller that will call the service
+            var controller = new createCustomerPaymentProfileController(request);
+            controller.Execute();
+
+            // get the response from the service (errors contained if any)
+            createCustomerPaymentProfileResponse response = controller.GetApiResponse();
+
+            // validate response 
+            if (response != null)
+            {
+                if (response.messages.resultCode == messageTypeEnum.Ok)
+                {
+                    if(response.messages.message != null)
+                    {
+                        Console.WriteLine("Success! Customer Payment Profile ID: " + response.customerPaymentProfileId);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Customer Payment Profile Creation Failed.");
+                    Console.WriteLine("Error Code: " + response.messages.message[0].code);
+                    Console.WriteLine("Error message: " + response.messages.message[0].text);
+                    if (response.messages.message[0].code == "E00039")
+                    {
+                        Console.WriteLine("Duplicate Payment Profile ID: " + response.customerPaymentProfileId);
+                    }
+                }
+            }
+            else
+            {
+                if (controller.GetErrorResponse().messages.message.Length > 0)
+                {
+                    Console.WriteLine("Customer Payment Profile Creation Failed.");
+                    Console.WriteLine("Error Code: " + response.messages.message[0].code);
+                    Console.WriteLine("Error message: " + response.messages.message[0].text);
+                }
+                else
+                {
+                    Console.WriteLine("Null Response.");
+                }
+            }
+
+            return response;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using AuthorizePayment.CustomerProfiles;
+using AuthorizeNet.Api.Contracts.V1;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using AuthorizePayment.PaymentTransactions;
+
+namespace AuthorizePayment
+{
+	internal class Program
+	{
+
+		static void prettyPrint(object response)
+		{
+			var responseStr = JsonConvert.SerializeObject(response);
+			string prettyStr;
+			prettyStr = JToken.Parse(responseStr).ToString(Formatting.Indented);
+			Console.WriteLine(prettyStr);
+		}
+
+		static void Main(string[] args)
+		{
+			ANetApiResponse response;
+			var apiLoginId = ConfigurationManager.AppSettings["APILoginID"];
+			var transactionKey = ConfigurationManager.AppSettings["TransactionKey"];
+			//CreateCustomerProfile.Run(apiLoginId, transactionKey, "test123@gmail.com");
+
+			//Customer Profile ID: 509095077
+			//Payment Profile ID: 514650103
+			//Shipping Profile ID: 510434233
+
+			var creditCard = new creditCardType
+			{
+				cardNumber = "4111111111111111",
+				expirationDate = "1028"
+			};
+			//response = UpdateCustomerPaymentProfile.Run(apiLoginId, transactionKey, "509095077", "514650103", creditCard);
+
+
+			//GetCustomerPaymentProfile.Run(apiLoginId, transactionKey, "509095077", "514650103");
+
+			//CreateCustomerPaymentProfile.Run(apiLoginId, transactionKey, "509095077");
+
+			//GetCustomerPaymentProfile.Run(apiLoginId, transactionKey, "509095077", "514650103");
+
+			//response = ChargeCreditCard.Run(apiLoginId, transactionKey, 100);
+
+			response = ChargeCustomerProfile.Run(apiLoginId, transactionKey, "509095077", "514650103", 100);
+			prettyPrint(response);
+		}
+	}
+}
